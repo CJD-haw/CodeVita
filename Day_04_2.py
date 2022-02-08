8ec9698ab682fcb67a11fa93339bf318d3da3f2b
Jeya Suriya <93221485+CJD-haw@users.noreply.github.com>
2022-02-08 16:08:13 +0530

    Created By Using Pycharm Text Editor
    Coding Problems With Solutions By Python Programming As I am preparing for TCS Code Vita 2022 Daily solve 5 coding problems for enriching the knowledge of problem solving.

--- Day_04_2.py
+++ Day_04_2.py
@@ -64,8 +64,12 @@ else:
 """
 
 year = int(input('Enter Year : '))
-
-if year % 4 == 0 or year % 400 == 0 and year % 100 != 0:
-    print('{} is a leap year'.format(year))
+if year % 4 == 0:
+    if year % 100 != 0:
+        print('{} is a leap year'.format(year))
+    elif year % 100 == 0 and year % 400 == 0:
+        print('{} is a leap year'.format(year))
+    else:
+        print('{} is not a leap year'.format(year))
 else:
     print('{} is not a leap year'.format(year))
