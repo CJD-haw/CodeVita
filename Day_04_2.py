7ec5b37c162a8758a2f89921bf067b3f57fd92de
Jeya Suriya <93221485+CJD-haw@users.noreply.github.com>
2022-02-08 16:11:15 +0530

    Created By Using Pycharm Text Editor
    Coding Problems With Solutions By Python Programming As I am preparing for TCS Code Vita 2022 Daily solve 5 coding problems for enriching the knowledge of problem solving.

--- Day_04_2.py
+++ Day_04_2.py
@@ -27,33 +27,25 @@ Explanation:
 
 """
 #python program to check if a year number taken from the user is a leap year or not, using nested if-else.
-
 num = int(input("Enter the year you want to check if is leap year or not: "))
 
 #take input year from the user to check if it is a leap year
-
 if(num%4 == 0):
 
  #check if the number is divisible by 4 and if true move to next loop
-
    if(num%100 == 0):
 
      #check if the input year is divisible by 100 and if true move to next loop
-
        if(num%400 == 0):
 
            print("The year {} is a leap year".format(num))
-
            #the input year is divisible by 4, 100 and 400, hence leap year.
 
        else:
-
            print("The year {} is Not a leap year".format(num))
 
    else:
-
        print("The year {} is a leap year".format(num))
-
        #if the number is divisible by both 4 and 100 it is a leap year
 
 else:
